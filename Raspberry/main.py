4347a45f7dfa8b326f7bfcf1705ca07e25dc3518
P4NZER88 <joao.vale@live.com.pt>
2020-03-17 12:44:29 +0000

    PIV was useful

--- Raspberry/main.py
+++ Raspberry/main.py
@@ -1,14 +1,31 @@
 import seeed_mlx90640
+import numpy as np
+from scipy.ndimage import gaussian_filter
+from scipy import ndimage
 import time
+
+sizeX = 32
+sizeY = 24
+TempT = 32
 
 def main():
     mlx = seeed_mlx90640.grove_mxl90640()
-    frame = [0] * 768
+    mlx.refresh_rate = seeed_mlx90640.RefreshRate.REFRESH_8_HZ
+    frame = [0] * sizeX * sizeY
     while True:
+        start = time.time()
         try:
             mlx.getFrame(frame)
+            framenp = np.array(frame)
+            framenp = framenp.reshape(sizeX, sizeY)
+            framegf = gaussian_filter(framenp, sigma=5)
+            labeled, nr_objects = ndimage.label(framegf > TempT)
+            print(nr_objects)
+            print(labeled)
         except ValueError:
             continue
+        end = time.time()
+        print("The time: %f"%(end - start))
 
 if __name__  == '__main__':
     main()
