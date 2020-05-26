788e45b5c0927a8f2ed2d79370ba145c68b7c827
jdpsc <49963197+jdpsc@users.noreply.github.com>
2020-05-26 14:04:47 +0100

    New App

--- TemperatureMonitor/app/src/main/java/com/example/temperaturemonitor/ListActivity.java
+++ TemperatureMonitor/app/src/main/java/com/example/temperaturemonitor/ListActivity.java
@@ -35,6 +35,7 @@ import java.util.List;
 
 public class ListActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
